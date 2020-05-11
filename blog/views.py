5af01116aa9105dad246fb2ff02c6c983fd51fbb
Artem <singlelogic@gmail.com>
2020-05-11 18:27:23 +0300

    sorted file 'views.py'

--- blog/views.py
+++ blog/views.py
@@ -11,19 +11,9 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blog/post_detail.html'
 
 
-class TagDetail(ObjectDetailMixin, View):
-    model = Tag
-    template = 'blog/tag_detail.html'
-
-
 class PostCreate(OblectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create.html'
-
-
-class TagCreate(OblectCreateMixin, View):
-    model_form = TagForm
-    template = 'blog/tag_create.html'
 
 
 class PostUpdate(ObjectUpdateMixin, View):
@@ -32,27 +22,37 @@ class PostUpdate(ObjectUpdateMixin, View):
     template = 'blog/post_update_form.html'
 
 
-class TagUpdate(ObjectUpdateMixin, View):
-    model = Tag
-    model_form = TagForm
-    template = 'blog/tag_update_form.html'
-
-
 class PostDelete(ObjectDeleteMixin, View):
     model = Post
     template = 'blog/post_delete_form.html'
     redirect_url = 'posts_list_url'
 
 
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagCreate(OblectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
+
+
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
+
+
 class TagDelete(ObjectDeleteMixin, View):
     model = Tag
     template = 'blog/tag_delete_form.html'
     redirect_url = 'tags_list_url'
-
-
-def posts_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
 
 
 def tags_list(request):
