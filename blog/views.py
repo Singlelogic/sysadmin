d6d93864cd4bae5bdbea2781b49483024428bbc5
Artem <singlelogic@gmail.com>
2020-05-11 11:14:23 +0300

    mixin 'ObjectUpdateMixin' created for update posts and tags

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.generic import View
 
 from .forms import TagForm, PostForm
 from .models import Post, Tag
-from .utils import ObjectDetailMixin, OblectCreateMixin
+from .utils import ObjectDetailMixin, OblectCreateMixin, ObjectUpdateMixin
 
 
 class PostDetail(ObjectDetailMixin, View):
@@ -26,27 +26,16 @@ class TagCreate(OblectCreateMixin, View):
     template = 'blog/tag_create.html'
 
 
-class TagUpdate(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        bound_form = TagForm(instance=tag)
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
 
-        return render(request, 'blog/tag_update_form.html', context={
-            'form': bound_form,
-            'tag': tag
-        })
 
-    def post(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        bound_form = TagForm(request.POST, instance=tag)
-
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_update_form.html', context={
-            'form': bound_form,
-            'tag': tag
-        })
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
 
 
 def posts_list(request):
