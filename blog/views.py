7667be584cdf491d45d43a7ddb48e81bd421e7a8
Artem <singlelogic@gmail.com>
2020-05-18 11:23:25 +0300

    search by title and body of posts

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .forms import TagForm, PostForm
 from .models import Post, Tag
@@ -34,9 +35,14 @@ class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
 
 
 def posts_list(request):
-    posts = Post.objects.all()
-    paginator = Paginator(posts, 2)
+    search_query = request.GET.get('search', '')
 
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
+    paginator = Paginator(posts, 2)
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
 
