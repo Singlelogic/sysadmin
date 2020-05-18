54bdefd3b9ec7f3e70cefe9bf4ff99d0ed3c1f05
Artem <singlelogic@gmail.com>
2020-05-18 13:41:18 +0300

    changed paginator, deleted links to the previous and next, added to the first and last

--- blog/views.py
+++ blog/views.py
@@ -36,33 +36,20 @@ class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
 
 def posts_list(request):
     search_query = request.GET.get('search', '')
-
     if search_query:
-        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+        posts = Post.objects.filter(Q(title__icontains=search_query) |
+                                    Q(body__icontains=search_query))
     else:
         posts = Post.objects.all()
 
     paginator = Paginator(posts, 2)
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
-
     is_paginated = page.has_other_pages()
-
-    if page.has_previous():
-        prev_url = '?page={}'.format(page.previous_page_number())
-    else:
-        prev_url = ''
-
-    if page.has_next():
-        next_url = '?page={}'.format(page.next_page_number())
-    else:
-        next_url = ''
 
     return render(request, 'blog/index.html', context={
         'page_object': page,
-        'is_paginated': is_paginated,
-        'prev_url': prev_url,
-        'next_url': next_url
+        'is_paginated': is_paginated
     })
 
 
