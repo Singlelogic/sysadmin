b064dafae2f7f1d3160ba213fba98e1c5288b36c
Artem <singlelogic@gmail.com>
2020-05-20 20:45:11 +0300

    ImageField added to Post model, post presentation templates changed. Added post and tag class to admin

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     body = models.TextField(blank=True, db_index=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(blank=True, upload_to='images/')
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
