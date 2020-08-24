58abc618cd5801b4bca2c5778e439e2da058e464
Artem <singlelogic@gmail.com>
2020-08-24 16:43:03 +0300

    russification of the account module

--- account/models.py
+++ account/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    date_of_birth = models.DateField(u'Дата рождения', blank=True, null=True)
+    photo = models.ImageField(u'Фото', upload_to='users/%Y/%m/%d', blank=True)
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
