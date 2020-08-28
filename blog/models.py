fdacbdfc2b19b79a04ad230cb3df1da1d0dfa937
Artem <singlelogic@gmail.com>
2020-08-28 10:40:39 +0300

    deleting an index(db_index=True) on a field 'body'

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ def gen_slug(s):
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, blank=True, unique=True)
-    body = models.TextField(blank=True, db_index=True)
+    body = models.TextField(blank=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
     image_preview = models.ImageField(upload_to='images/')
